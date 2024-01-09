2e9f9da69f1db5665a3fc3eb6772871e4e490d1d
Alobeast <alois.beard@gmail.com>
2024-01-09 16:05:25 +0100

    preload images in the background

--- js/gallery.js
+++ js/gallery.js
@@ -6,17 +6,17 @@ function activateGallery() {
   let description = galleryInfo.querySelector(".description");
 
   thumbnails.forEach(function (thumbnail) {
+    let newImageSrc = thumbnail.dataset.largeVersion;
+    let newImageAlt = thumbnail.alt;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
     thumbnail.addEventListener("click", function () {
-      let newImageSrc = thumbnail.dataset.largeVersion;
-      let newImageAlt = thumbnail.alt;
       mainImage.setAttribute("src", newImageSrc);
       mainImage.setAttribute("alt", newImageAlt);
 
       formerThumbnail = document.querySelector(".current");
       formerThumbnail.classList.remove("current");
       thumbnail.parentNode.classList.add("current");
-
-
 
       title.innerHTML = thumbnail.dataset.title;
       description.innerHTML = thumbnail.dataset.description;
